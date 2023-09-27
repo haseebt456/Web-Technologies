5fb68a9fcaea572ec66ee5164a2e3a616f25b1ef
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-09-27 21:02:22 +0500

    started javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,36 @@
+/*
+let mark_weight=78;
+let mark_height=1.69;
+let john_weight=92;
+let john_height=1.92;
+let mark_BMI=mark_weight/mark_height*mark_height;
+let john_BMI=john_weight/john_height*john_height;
+let greater_BMI=false;
+if(mark_BMI > john_BMI)
+{
+    greater_BMI=true;
+}
+console.log(greater_BMI);*/
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+  ];
+  console.log(complexArray.length);
