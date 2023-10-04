ccfb6bc9c91e14aa0ce5b6d1a8dfb928820b44ef
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-10-04 21:56:10 +0500

    Awesome books'

--- JavaScript/lab 2/index.js
+++ JavaScript/lab 2/index.js
@@ -1,10 +1,26 @@
 function disapear()
 {
-    const div=document.getElementById('div');
-    const para = div.querySelector('#para');
-    para.style.display='none';
+    const div=document.getElementById('container');
+    div.style.display = 'none';
+}
 
-    const h = document.createElement('h1')
-    h.innerText = "Hello Pakistan"
-    div.appendChild(h);
+function inputData()
+{
+    var title=document.getElementById('title').value
+    var author=document.getElementById('author').value
+    const t = document.createElement('p')
+    const a= document.createElement('p')
+    const btn = document.createElement('button')
+    const div = document.createElement('div')
+    t.innerText = title;
+    a.innerText = author;
+    btn.innerText = "remove";
+    btn.addEventListner("click",()=> {
+        div.querySelector("#p").style.display = 'none';
+    }); 
+    const cont=document.getElementById('container');
+    div.appendChild(t);
+    div.appendChild(a);
+    div.appendChild(btn);
+    cont.appendChild(div);
 }
