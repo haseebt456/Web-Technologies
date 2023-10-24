7f4a3a33a2fe13e226627a6ca4c23cef54cd8611
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-10-24 22:55:58 +0500

    added condition for equal numbers

--- JavaScript/Dice game/index.js
+++ JavaScript/Dice game/index.js
@@ -4,10 +4,14 @@
       var randomSRC2="./images/dice"+randomnumber2+".png";
       var image1=document.querySelector('.img1');
       var image2=document.querySelector('.img2');
-     
+      image1.setAttribute('src',randomSRC1);
+      image2.setAttribute('src',randomSRC2);
       if(randomnumber1 > randomnumber2){
       document.querySelector('h1').innerHTML='Player 1 wins';
       }
-      else{
+      else if(randomnumber2 > randomnumber1){
       document.querySelector('h1').innerHTML='Player 2 wins';
+      }
+      else {
+        document.querySelector('h1').innerHTML="DRAW!";
       }
