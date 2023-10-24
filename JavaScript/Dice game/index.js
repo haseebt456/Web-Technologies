b5bfed9c8dd1b7b663bf2e4e29b0c6860bdb1e95
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-10-24 22:52:45 +0500

    raplace ifs with simple strings

--- JavaScript/Dice game/index.js
+++ JavaScript/Dice game/index.js
@@ -1,55 +1,10 @@
       var randomnumber1=Math.floor(Math.random()*6)+1;
       var randomnumber2=Math.floor(Math.random()*6)+1;
+      var randomSRC1="./images/dice"+randomnumber1+".png";
+      var randomSRC2="./images/dice"+randomnumber2+".png";
       var image1=document.querySelector('.img1');
       var image2=document.querySelector('.img2');
-      if(randomnumber1==1)
-      {
-        image1.setAttribute('src','./images/dice1.png')
-      }
-      if(randomnumber1==2)
-      {
-        image1.setAttribute('src','./images/dice2.png')
-      }
-      if(randomnumber1==3)
-      {
-        image1.setAttribute('src','./images/dice3.png')
-      }
-      if(randomnumber1==4)
-      {
-        image1.setAttribute('src','./images/dice4.png')
-      }
-      if(randomnumber1==5)
-      {
-        image1.setAttribute('src','./images/dice5.png')
-      }
-      if(randomnumber1==6)
-      {
-        image1.setAttribute('src','./images/dice6.png')
-      }
-      if(randomnumber2 == 1)
-      {
-        image2.setAttribute('src','./images/dice1.png');
-      }
-      if(randomnumber2 == 2)
-      {
-        image2.setAttribute('src','./images/dice2.png');
-      }
-      if(randomnumber2 == 3)
-      {
-        image2.setAttribute('src','./images/dice3.png');
-      }
-      if(randomnumber2 == 4)
-      {
-        image2.setAttribute('src','./images/dice4.png');
-      }
-      if(randomnumber2 == 5)
-      {
-        image2.setAttribute('src','./images/dice5.png');
-      }
-      if(randomnumber2 == 6)
-      {
-        image2.setAttribute('src','./images/dice6.png');
-      }
+     
       if(randomnumber1 > randomnumber2){
       document.querySelector('h1').innerHTML='Player 1 wins';
       }
