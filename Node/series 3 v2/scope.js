3248870490d5c2ad07ea6d00af15f4291e222450
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-23 20:45:46 +0500

    Scope of variable vs obj

---
+++ Node/series 3 v2/scope.js
@@ -0,0 +1,18 @@
+let a=10;
+
+function increment(x){
+    x++;
+}
+increment(a);
+increment(a);
+console.log(a);
+
+let y = {value : 10};
+
+function incrementObject(y)
+{
+    y.value++;
+}
+incrementObject(y);
+incrementObject(y);
+console.log(y);
