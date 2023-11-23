c41a63a9a7a4df778ade8afba2bc8cc819150acd
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-23 19:25:47 +0500

    different variables tested

---
+++ Node/series 3 v2/letvsVarvsConst.js
@@ -0,0 +1,8 @@
+let name= 'haseeb';
+var age=20;
+const weight=60;
+//const weight=60;//cannot do this
+var age=21;//var can be declared again
+//let name = 'junaid'; cannot do this
+name='talha';
+console.log(name);
