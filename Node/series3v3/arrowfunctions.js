23a46f2eceaa6e92dab178fdbbe76ea209d6daff
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-24 17:35:15 +0500

    Arrow functions

---
+++ Node/series3v3/arrowfunctions.js
@@ -0,0 +1,10 @@
+function test(name){
+    if(name == "haseeb")
+    return true;
+else 
+return false;//this can also be written as following
+}
+
+const testa= name => (name=="haseeb" ? true:false);//this is  functio with   input name
+
+console.log(testa("ali"));
