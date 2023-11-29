39532d1703b517402d00cc4a0074d812b2871f55
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-30 00:42:13 +0500

    Add async example

--- JavaScript/callback by harry/index.js
+++ JavaScript/callback by harry/index.js
@@ -1,5 +1,14 @@
 //syncronous programming - runs step by step in sequence
-let a = prompt("What is ypur name?");
+/* let a = prompt("What is ypur name?");
 let b = prompt("What is your age?");
 
 console.log("Age of "+a+" is "+b);
+ */
+
+//Assync programming - actions that are initiates now and they finish later
+
+console.log("start");
+setTimeout(function(){
+    console.log("Hey, i am haseeb");
+});
+console.log("end");
