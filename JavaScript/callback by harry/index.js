2d1d22841ce1c8369db1a176d9b42b62b86230b0
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-30 19:22:51 +0500

    Add call back example

--- JavaScript/callback by harry/index.js
+++ JavaScript/callback by harry/index.js
@@ -7,8 +7,36 @@ console.log("Age of "+a+" is "+b);
 
 //Assync programming - actions that are initiates now and they finish later
 
-console.log("start");
+/* console.log("start");
 setTimeout(function(){
     console.log("Hey, i am haseeb");
 });
 console.log("end");
+ */
+//output
+//start
+//end
+//hey,i am haseeb
+//But code didn't stopped rather it has just scheduled that settimout action
+
+//Callback
+
+function loadscript(src,Callback)
+{
+    var script = document.createElement('script');
+    script.src = src;
+    script.onload = function (){
+        console.log("loaded script with src : "+src);
+        Callback();
+    }
+    document.body.appendChild(script);
+};
+
+function hello(){
+    alert("hello")
+}
+function mycallback(){
+    alert("Call back");
+};
+
+loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",mycallback);
