597d8155f4253aef3879cbcce150f9a9e4465406
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 13:54:21 +0500

    Replace multiple handlers with promise.all

--- JavaScript/promises/Promise-API/index.js
+++ JavaScript/promises/Promise-API/index.js
@@ -15,7 +15,7 @@ let p3 = new Promise((resolve,reject)=>{
     },3000)
 });
 
-p1.then((value)=>{
+/* p1.then((value)=>{
     console.log(value);
 })
 p2.then((value)=>{
@@ -23,6 +23,14 @@ p2.then((value)=>{
 })
 p3.then((value)=>{
     console.log(value);
-})
+}) */
+
 //in real life we don't know how much it will take for a promise to complete
 //to run all promise together we will use promise.all
+
+let promise_all=Promise.all([p1,p2,p3]);//values of all promises will be stored in promise_all array
+promise_all.then((value)=>{
+    for(let i=0;i<value.length;i++){
+        console.log(value[i]);
+    }
+})
