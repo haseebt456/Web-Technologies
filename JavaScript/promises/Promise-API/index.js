3d2b02df617a950bfa37e53a47cb2a332a3fa37d
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 14:21:18 +0500

    Add seperate example for rejection

--- JavaScript/promises/Promise-API/index.js
+++ JavaScript/promises/Promise-API/index.js
@@ -6,8 +6,7 @@ let p1 = new Promise((resolve,reject)=>{
 })
 let p2 = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        //resolve("value 2");
-        reject(new Error("Promise.all will not work"));
+        resolve("value 2");
     },2000)
 })
 let p3 = new Promise((resolve,reject)=>{
@@ -16,6 +15,11 @@ let p3 = new Promise((resolve,reject)=>{
     },3000)
 });
 
+let p_rejected = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Error occured"));
+    })
+})
 /* p1.then((value)=>{
     console.log(value);
 })
@@ -30,14 +34,21 @@ p3.then((value)=>{
 //to run all promise together we will use promise.all
 //If any of the promise is rejected then promise.all will not work 
 let promise_all=Promise.all([p1,p2,p3]);//values of all promises will be stored in promise_all array
+
 promise_all.then((value)=>{
+    for(let i=0;i<value.length;i++){
+        console.log(value[i]);
+    }
+});
+let promise_all_reject=Promise.all([p1,p_rejected,p3]);//values of all promises will be stored in promise_all array
+promise_all_reject.then((value)=>{
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
     }
 });
 
 //to solve promise.all rejection issue we will use .allSettled which will not only give value but also status
-let promise_all_set = Promise.allSettled([p1,p2,p3]);
+let promise_all_set = Promise.allSettled([p1,p_rejected,p3]);
 promise_all_set.then((value)=>{
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
