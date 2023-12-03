01ceffaf1f9dc6f88e6c29f94fc40bbb70c7867f
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 17:46:07 +0500

    Add await example

--- JavaScript/Async_Await/index.js
+++ JavaScript/Async_Await/index.js
@@ -11,13 +11,18 @@ let lahoreWeather = new Promise((resolve,reject)=>{
 let karachiWeather = new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve("25 degree");
-    },2000)
-
-    lahoreWeather.then(alert);
+    },5000)
 });
+/* lahoreWeather.then(alert);
+karachiWeather.then(alert);
+ */
+let lahoreW = await lahoreWeather;
+let karachiW = await karachiWeather;
+return [lahoreW,karachiW];
 }
-
-getWeather();
+console.log("welcome to weather app");
+let a=getWeather();
+console.log(a);
 //in real we will not create new promises like this rather promises will be returned automaticaly like in mongoose
 //we are trying to mimick 
 /* async function myfunction()
