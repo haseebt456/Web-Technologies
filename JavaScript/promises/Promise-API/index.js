2a0dd60f08635afa1e225962bbdb0c4df7d51f74
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 13:57:32 +0500

    Add reject to show howpromise.all respond

--- JavaScript/promises/Promise-API/index.js
+++ JavaScript/promises/Promise-API/index.js
@@ -6,7 +6,8 @@ let p1 = new Promise((resolve,reject)=>{
 })
 let p2 = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        resolve("value 2");
+        //resolve("value 2");
+        reject(new Error("Promise.all will not work"));
     },2000)
 })
 let p3 = new Promise((resolve,reject)=>{
@@ -27,7 +28,7 @@ p3.then((value)=>{
 
 //in real life we don't know how much it will take for a promise to complete
 //to run all promise together we will use promise.all
-
+//If any of the promise is rejected then promise.all will not work 
 let promise_all=Promise.all([p1,p2,p3]);//values of all promises will be stored in promise_all array
 promise_all.then((value)=>{
     for(let i=0;i<value.length;i++){
