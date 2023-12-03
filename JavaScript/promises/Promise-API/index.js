d204c9846d9002605b2f0747e7b0b34be3d8ee23
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 14:13:30 +0500

    Add .allSetled to handle rejection

--- JavaScript/promises/Promise-API/index.js
+++ JavaScript/promises/Promise-API/index.js
@@ -34,4 +34,12 @@ promise_all.then((value)=>{
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
     }
-})
+});
+
+//to solve promise.all rejection issue we will use .allSettled which will not only give value but also status
+let promise_all_set = Promise.allSettled([p1,p2,p3]);
+promise_all_set.then((value)=>{
+    for(let i=0;i<value.length;i++){
+        console.log(value[i]);
+    }
+}) 
