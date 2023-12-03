f38a0b933212acd80a17272fcb5e8cad4231008e
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 14:31:07 +0500

    Add console messages to identify results

--- JavaScript/promises/Promise-API/index.js
+++ JavaScript/promises/Promise-API/index.js
@@ -36,11 +36,13 @@ p3.then((value)=>{
 let promise_all=Promise.all([p1,p2,p3]);//values of all promises will be stored in promise_all array
 
 promise_all.then((value)=>{
+    console.log("Simple .all to hanlde all promsie together")
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
     }
 });
 let promise_all_reject=Promise.all([p1,p_rejected,p3]);//values of all promises will be stored in promise_all array
+console.log("In case of rejection promise will not work now");
 promise_all_reject.then((value)=>{
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
@@ -50,7 +52,15 @@ promise_all_reject.then((value)=>{
 //to solve promise.all rejection issue we will use .allSettled which will not only give value but also status
 let promise_all_set = Promise.allSettled([p1,p_rejected,p3]);
 promise_all_set.then((value)=>{
+    console.log(".allSetled that will show result whether a rejection occured");
     for(let i=0;i<value.length;i++){
         console.log(value[i]);
     }
-}) 
+});
+
+let promise_race = Promise.race([p1,p2,p3]);
+
+promise_race.then((value)=>{
+    console.log("Promise.race showing value of earliest ressolved promise");
+    console.log(value);
+})
