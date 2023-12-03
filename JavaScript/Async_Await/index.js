f660cfff0851af319aa6940d8d3637c7394f7c4a
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-12-03 17:39:33 +0500

    Add two mimicking promises

--- JavaScript/Async_Await/index.js
+++ JavaScript/Async_Await/index.js
@@ -2,10 +2,29 @@
 //Async functionalways return promise
 //async ensures function returns a promise and wraps non-promise in it
 
-async function myfunction(){
+async function getWeather(){
+let lahoreWeather = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("20 degree");
+    },1000)
+});
+let karachiWeather = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("25 degree");
+    },2000)
+
+    lahoreWeather.then(alert);
+});
+}
+
+getWeather();
+//in real we will not create new promises like this rather promises will be returned automaticaly like in mongoose
+//we are trying to mimick 
+/* async function myfunction()
+{
     return 5;
 };
 
 myfunction().then((value)=>{
     alert(value);
-})
+}); */
