e0d50f2119359b83564c0f1cfb63fab9c23fc012
haseebt456 <m.haseebtufail@gmail.com>
2023-12-06 20:10:11 +0500

    Add get method

--- JavaScript/Fetch_API/index.js
+++ JavaScript/Fetch_API/index.js
@@ -1,22 +1,29 @@
 
-const Addrecipe = async ()=>{
+const Addrecipe = async (resipe)=>{
     let options = {
         method: 'POST',
         headers: {
             "content-type": "Application/json"
         },
-        body: JSON.stringify({
-            title: "How to make tea",
-            body: "Add milk then tea and sugar and cook untill it boils"
-        })
+        body: JSON.stringify(resipe)
     }
     let p = await fetch("https://usman-fake-api.herokuapp.com/api/recipes",options);
     let response = await p.json();
     return response;
 };
-
+const getRecipe = async (_id)=>{
+  
+    let p = await fetch("https://usman-fake-api.herokuapp.com/api/recipes"+_id);
+    let response = await p.json();
+    return response;
+}
 const mainFunc = async ()=>{
-    let recipe=await Addrecipe();
-    console.log(recipe);
+    /* let resipe = {
+        title:"How to make Coffee",
+        body: "Add hot milk to cup and add coffe and mix it for 2 min"
+    };
+    let recipe=await Addrecipe(resipe);
+    console.log(recipe); */
+   console.log(getRecipe("65708b18b06efd0014dee8ba"));
 }
 mainFunc();
