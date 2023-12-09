2bd5b133474a540189c15a7cd7462e7665bd1ac0
haseebt456 <m.haseebtufail@gmail.com>
2023-12-09 22:36:29 +0500

    adde functionality to create product

--- mongoDB/models/product.js
+++ mongoDB/models/product.js
@@ -4,12 +4,8 @@ const poroductSchema = mongoose.Schema({
     title:String,
     price:Number,
     tags:[string],
-    slug: {
-        type:string,
-        lowercase:true,
-    },
 });
 //defining schema as mongoDB is schema less
 
-const product = mongoose.model("ProductModel",poroductSchema);//model of schema
+const product = mongoose.model("products",poroductSchema);//model of schema
 module.exports = ProductModel;
--- mongoDB/productOperations.js
+++ mongoDB/productOperations.js
@@ -1,6 +1,12 @@
-
-const createProduct = async (title,price,tags,slug) =>{
+const ProductModel = require("./models/product");
+const createProduct = async (title,price,tags) =>{
     console.log("create product");
+    let product = new ProductModel();
+    product.title = title;
+    product.price = price;
+    product.tags = tags;
+    await product.save();
+    return product;
 } 
 
 module.exports.createProduct = createProduct;
