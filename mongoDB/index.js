3680fbbd067ffdbca0a00b5faf6074584f67cb39
haseebt456 <m.haseebtufail@gmail.com>
2023-12-09 23:55:44 +0500

    execute get products

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const {createProduct} = require("./productOperations")
+const {createProduct} = require("./productOperations");
+const {getAllProducts} = require("./productOperations")
 app.use(express.json());
 mongoose.connect("mongodb://0.0.0.0:27017/testDB",{
     useNewUrlParser: true, useUnifiedTopology: true
 })
 .then(async ()=>{
     console.log("connection to mongoDb sucessful");
-    let product=await createProduct("Laptop",100000,["black","lenovo"]);
-    console.log(product);
+    //let product=await createProduct("Laptop",100000,["black","lenovo"]);
+    //console.log(product);;
+    let products = await getAllProducts();;
+    console.log(products);
 })
 .catch((err)=>{
     console.log("Error connecting");
